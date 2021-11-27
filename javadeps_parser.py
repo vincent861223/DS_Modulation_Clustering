dc7697e0e308ae75ab3b8bfec6c2383257778542
Xinrong-Huang <xinrongh@andrew.cmu.edu>
2021-11-27 01:11:01 -0800

    fix jdeps issue

--- javadeps_parser.py
+++ javadeps_parser.py
@@ -9,7 +9,7 @@ class Javadeps_parser:
         self.dependencies = defaultdict(set)
         self.valid_imports = 'org.mockito'
 
-    def find_dependencies(self, source):
+    def find_dependencies(self, fname, source):
         ast = javalang.parse.parse(source)
         # print(ast)
 
@@ -18,35 +18,38 @@ class Javadeps_parser:
             if not imp.path.startswith(self.valid_imports):
                 continue
             else:
-                self.dependencies[ast.package.name].add(imp.path)
+                self.dependencies[fname].add(imp.path)
 
-            
+
     def parse_files(self):
         for file in self.allFiles:
+            filename = file.replace('/', '.')
+            fname = 'org.' + filename[:-5]
+
             f = open(file)
             source = f.read()
             f.close()
 
-            self.find_dependencies(source)
+            self.find_dependencies(fname, source)
         print(self.dependencies)
 
     def get_java_files(self, dirName):
-        # create a list of file and sub directories 
-        # names in the given directory 
+        # create a list of file and sub directories
+        # names in the given directory
         listOfFile = os.listdir(dirName)
         allFiles = list()
         # Iterate over all the entries
         for entry in listOfFile:
             # Create full path
             fullPath = os.path.join(dirName, entry)
-            # If entry is a directory then get the list of files in this directory 
+            # If entry is a directory then get the list of files in this directory
             if os.path.isdir(fullPath):
                 allFiles = allFiles + self.get_java_files(fullPath)
             else:
                 if fullPath.endswith(".java"):
                     allFiles.append(fullPath)
 
-        self.allFiles = allFiles           
+        self.allFiles = allFiles
         return allFiles
 
 if __name__ == '__main__':
